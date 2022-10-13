bc67ab53c4cd1876416532e47cd9496cebdc46b0
VianeyAR <l19350734@tuxtepec.tecnm.mx>
2022-10-13 07:06:24 -0500

    Coneccion

--- controllers/Usuarios.js
+++ controllers/Usuarios.js
@@ -1,9 +1,28 @@
 const{reques, response} = require("express")
+const pool = require("../db/connection")
 
 
-const getUsers = (req= reques, res = response) => {
-    console.log("Funcion getUsers")
-    res.json({msg: "Funcion getUsers"})
+const getUsers = async (req = reques, res = response) => {
+    let conn
+    try{
+        conn = await pool.getConnection() //realizamos la conexion
+        
+        //generamos la consulta
+        const users = await conn.query("SELECT * FROM Usuarios", (error) => {if (error) throw error})
+
+        if(!users){ // En caso de no haber registros lo informamos
+            res.status(404).json({msg: "NO existen usuarios registrados"})
+            return
+        }
+        res.json({users})
+    }catch (error){
+
+        console.log(error)
+        res.status(500).json({msg: error}) //informamos del error
+    } finally{
+        if (conn) conn.end() //termina la conexion
+
+    }
 }
 
 module.exports = {getUsers}
